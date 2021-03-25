1bbdcb18324c96ab2252e35ba1a4a4f65ec55138
Botond Turai <turai.botond@edu.bme.hu>
2021-03-25 22:29:39 +0100

    Türözés még mindig nem megy

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -1,5 +1,21 @@
 ﻿#include "framework.h"
 
+// NYILATKOZAT
+// ---------------------------------------------------------------------------------------------
+// Nev    : Turai Botond
+// Neptun : SR9IVY
+// ---------------------------------------------------------------------------------------------
+// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
+// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
+// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a
+// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb
+// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem,
+// azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
+// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.
+// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
+// negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
+//=============================================================================================
+
 const char* const vertexSource = R"(
 	#version 330				// Shader 3.3
 	precision highp float;		// normal floats, makes no difference on desktop computers
@@ -22,18 +38,15 @@ const char* const fragmentSource = R"(
 
 GPUProgram gpuProgram;
 
-//Konstansok
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const int NODES = 50;															//
-const float TELITETTSEG = 0.05f;	//Number of real edges						//
-const int CIRCLE_RESOLUTION = 16;	//											//
-const float RADIUS = 0.03f;			//Circle radius								// 
-const size_t EDGES = 61;			//(((NODES - 1)* NODES) / 2)* TELITETTSEG;	//
-const float DIST = 0.4f;			//Prefered distance							//	
-const float SURLODAS = 0.001f;													//
-const float DT = 0.008f;														//
-const float HIBAHATAR = 0.05f;													//
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const int NODES = 50;
+const float TELITETTSEG = 0.05f;
+const int CIRCLE_RESOLUTION = 16;
+const float RADIUS = 0.03f;
+const size_t EDGES = 61;
+const float DIST = 0.4f;
+const float SURLODAS = 0.01f;
+const float DT = 0.000008f;
+const float HIBAHATAR = 0.02f;
 bool dinSim = false;
 
 vec3 trf(vec2 inp, float nagyitas = 1.0f) {
@@ -43,18 +56,31 @@ vec3 trf(vec2 inp, float nagyitas = 1.0f) {
 	ret.y = inp.y / ret.z;
 	return ret * nagyitas;
 }
+vec3 hip(const vec2 inp) {
+	vec3 ret;
+	ret.x = inp.x;
+	ret.y = inp.y;
+	ret.z = sqrtf(1.0f + inp.x * inp.x + inp.y * inp.y);
+	return ret;
+}
+void sikra(vec3& inp) {
+	inp.x = inp.x / inp.z;
+	inp.y = inp.y = inp.z;
+}
 vec3 Eukl(vec3 hip) {
 	vec3 ret;
 	ret.x = hip.x * hip.z;
 	ret.y = hip.y * hip.z;
 	return ret ;
 }
-float lorenz(vec3 a, vec3 b) {
-	return a.x * b.x + a.y * b.y - a.z * b.z;
+float lorenz(const vec3& a, const vec3& b) {
+	return (a.x * b.x) + (a.y * b.y) - (a.z * b.z);
 }
-float d(const vec3& a, const vec3& b) {
-	return acoshf(-lorenz(a, b)); //Ahol a pos egy vec3 típusú tagváltozó. (x,y,w)
+float dd(const vec3& a, const vec3& b) {
+	return acoshf(-lorenz(a, b));
 }
+
+
 struct grafPont {
 	vec3 hip;
 	vec3 pos;
@@ -105,8 +131,9 @@ public:
 			int n1 = rand() % NODES;
 			int n2 = rand() % NODES;
 			if (n1 != n2) {
-				if (szMtx[min(n1, n2)][max(n1, n2)] == false) {
-					szMtx[min(n1, n2)][max(n1, n2)] = true;
+				
+				if (szMtx[(n1 < n2) ? n1 : n2][(n1 < n2) ? n2 : n1] == false) {
+					szMtx[(n1 < n2) ? n1 : n2][(n1 < n2) ? n2 : n1] = true;
 					--szukseges_el;
 				}
 			}
@@ -116,21 +143,20 @@ public:
 		if (idx >= NODES) throw "Tulindexeles";
 		return nodes[idx];
 	}
-	grafPont* edgeAt(size_t idx) {
+	bool edgeAt(const size_t idx, size_t& a, size_t& b) {
 		if (idx >= EDGES) throw "Sok lesz az az el!";
 		size_t cnt = 0;
 		for (size_t x = 0; x < NODES - 1; ++x) {
 			for (size_t y = x + 1; y < NODES; ++y) {
-				if (szMtx[x][y] == true) ++cnt;
-				if (cnt == idx) {
-					grafPont* ret = new grafPont[2];
-					ret[0] = nodes[x];
-					ret[1] = nodes[y];
-					return ret;
+				if (cnt == idx && szMtx[x][y] == true) {
+					a = x;
+					b = y;
+					return true;
 				}
+				if (szMtx[x][y] == true) { ++cnt; }
 			}
 		}
-		return nullptr;
+		return false;
 	}
 	void prepareCircle() {
 		if(nodeVao==0)
@@ -142,7 +168,7 @@ public:
 		float vertices[NODES * CIRCLE_RESOLUTION * 2];
 		for (size_t i = 0; i < NODES; ++i) {
 			float x = nodes[i].pos.x, y = nodes[i].pos.y;
-			//Az alábbi forciklus forrása: https://vik.wiki/Sz%C3%A1m%C3%ADt%C3%B3g%C3%A9pes_grafika_h%C3%A1zi_feladat_tutorial
+			//A forciklus forrasa: https://vik.wiki/Sz%C3%A1m%C3%ADt%C3%B3g%C3%A9pes_grafika_h%C3%A1zi_feladat_tutorial
 			for (size_t j = 0; j < CIRCLE_RESOLUTION; j++) {
 				float angle = float(j) / float(CIRCLE_RESOLUTION) * 2.0f * float(M_PI);
 				vec2 p(x + RADIUS * cosf(angle), y + RADIUS * sinf(angle));
@@ -157,13 +183,13 @@ public:
 	}
 	void drawCircle() {
 		int location = glGetUniformLocation(gpuProgram.getId(), "color");
-		glUniform3f(location, 0.0f, 1.0f, 0.0f); // 3 floats
+		glUniform3f(location, 0.0f, 1.0f, 0.0f);
 		float MVPtransf[4][4] = { 1.8f, 0, 0, 0, 0, 1.8f, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
 		location = glGetUniformLocation(gpuProgram.getId(), "MVP");
 		glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);
-		glBindVertexArray(nodeVao);  // Draw call
+		glBindVertexArray(nodeVao); 
 		for (size_t i = 0; i < NODES; ++i) {
-			glDrawArrays(GL_TRIANGLE_FAN, i * CIRCLE_RESOLUTION /*startIdx*/, CIRCLE_RESOLUTION /*# Elements*/);
+			glDrawArrays(GL_TRIANGLE_FAN, i * CIRCLE_RESOLUTION, CIRCLE_RESOLUTION);
 		}
 	}
 	void prepareEdges() {
@@ -173,32 +199,32 @@ public:
 		glBindBuffer(GL_ARRAY_BUFFER, edgeVbo);
 		float vertices[EDGES * 4];
 		for (size_t i = 0; i < EDGES; ++i) {
-			grafPont* tmp = edgeAt(i);
-			if (tmp == nullptr) continue;
-			vertices[i * 4] = tmp[0].hip.x;
-			vertices[i * 4 + 1] = tmp[0].hip.y;
-			vertices[i * 4 + 2] = tmp[1].hip.x;
-			vertices[i * 4 + 3] = tmp[1].hip.y;
-			delete[] tmp;
+			size_t idxA;
+			size_t idxB;
+			if (edgeAt(i, idxA, idxB)) {
+				vertices[i * 4] = nodes[idxA].hip.x;
+				vertices[i * 4 + 1] = nodes[idxA].hip.y;
+				vertices[i * 4 + 2] = nodes[idxB].hip.x;
+				vertices[i * 4 + 3] = nodes[idxB].hip.y;
+			}
 		}
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
-		glEnableVertexAttribArray(0);  // AttribArray 0
+		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 	}
 	void drawEdges() {
 		int location = glGetUniformLocation(gpuProgram.getId(), "color");
-		glUniform3f(location, 0.0f, 0.0f, 1.0f); // 3 floats
+		glUniform3f(location, 0.0f, 0.0f, 1.0f);
 		float MVPtransf[4][4] = { 1.8f, 0, 0, 0, 0, 1.8f, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 };
 		location = glGetUniformLocation(gpuProgram.getId(), "MVP");
 		glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);
-		glBindVertexArray(edgeVao);  // Draw call
-		glDrawArrays(GL_LINES, 0, EDGES * 2);///startIdx///# Elements/
+		glBindVertexArray(edgeVao);
+		glDrawArrays(GL_LINES, 0, EDGES * 2);
 	}
 	void magic() {
 		int legjobb = elmetszetek();
-		const int kezdoall = legjobb;
 		grafPont* gp = nodes;
-		while(legjobb > (kezdoall * 0.7)) {
+		while(legjobb >= elmetszetek()) {
 			nodes = new grafPont[NODES];
 			if (elmetszetek() < legjobb) {
 				delete[] gp;
@@ -213,30 +239,29 @@ public:
 		int sum = 0;
 		for (size_t i = 0; i < EDGES - 1; ++i) {
 			for (size_t j = i + 1; j < EDGES; ++j) {
-				grafPont* egyik = edgeAt(i);
-				grafPont* masik = edgeAt(j);
-				if (metszikEgymast(egyik, masik)) ++sum;
-				delete[] egyik;
-				delete[] masik;
+				size_t idxA, idxB, idxC, idxD;
+				if(edgeAt(i,idxA, idxB) && edgeAt(j, idxC, idxD)){
+					if (metszikEgymast(nodes[idxA], nodes[idxB], nodes[idxC], nodes[idxD])) 
+						++sum;
+				}
 			}
 		}
 		return sum;
 	}
 	/*
-	* A kód alapja a http://flassari.is/2008/11/line-line-intersection-in-cplusplus/ oldalról származik
-	* melyet késõbb kissé módosítottam.
+	* A kod alapja a http://flassari.is/2008/11/line-line-intersection-in-cplusplus/ oldalrol szarmazik
+	* melyet kesobb kisse modositottam.
 	*/
-	bool metszikEgymast(grafPont* a, grafPont* b) {
-		if (a == nullptr || b == nullptr) return false;
-		float x1 = a[0].pos.x, x2 = a[1].pos.x, x3 = b[0].pos.x, x4 = b[1].pos.x;
-		float y1 = a[0].pos.y, y2 = a[1].pos.y, y3 = b[0].pos.y, y4 = b[1].pos.y;
+	bool metszikEgymast(const grafPont& pa, const grafPont& pb, const grafPont& pc, const grafPont& pd ) {
+		float x1 = pa.pos.x, x2 = pb.pos.x, x3 = pc.pos.x, x4 = pd.pos.x;
+		float y1 = pa.pos.y, y2 = pb.pos.y, y3 = pc.pos.y, y4 = pd.pos.y;
 		float d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
 		if (d == 0) return false;
 		float pre = (x1 * y2 - y1 * x2), post = (x3 * y4 - y3 * x4);
 		float x = (pre * (x3 - x4) - (x1 - x2) * post) / d;
 		float y = (pre * (y3 - y4) - (y1 - y2) * post) / d;
-		if (x < min(x1, x2) || x > max(x1, x2) || x < min(x3, x4) || x > max(x3, x4)) return false;
-		if (y < min(y1, y2) || y > max(y1, y2) || y < min(y3, y4) || y > max(y3, y4)) return false;
+		if (x < ((x1<x2)?x1:x2) || x > ((x1>x2)?x1:x2) || x < ((x3<x4)?x3:x4) || x > ((x3>x4)?x3:x4)) return false;
+		if (y < ((y1<y2)?y1:y2) || y > ((y1>y2)?y1:y2) || y < ((y3<y4)?y3:y4) || y > ((y3>y4)?y3:y4)) return false;
 		return true;
 	}
 	float calcNode(const size_t idx) {
@@ -245,7 +270,7 @@ public:
 		p.ujpos = p.pos;
 		p.ero = p.ero * 0;
 		p.v = p.v * 0;
-		for (size_t i = 0; i < NODES; ++i){
+		for (size_t i = 0; i < NODES; ++i) {
 			if (i == idx) continue;
 			grafPont& q = nodes[i];
 			F(p, q, szMtx[(idx > i) ? i : idx][(idx > i) ? idx : i]);
@@ -253,7 +278,7 @@ public:
 		grafPont kozepe;
 		kozepe.pos.x = 0;
 		kozepe.pos.y = 0;
-		for(int i=0; i<5; ++i){
+		for (int i = 0; i < 5; ++i) {
 			vec3 kul(kozepe.pos - p.pos);
 			kul = kul * 0.001f;
 			p.ero = p.ero + kul;
@@ -264,15 +289,15 @@ public:
 		p.ujpos = p.ujpos + (p.v * DT);
 		return sqrtf(p.v.x * p.v.x + p.v.y * p.v.y);
 	}
-	void F(grafPont& a, grafPont& b,const bool szomszedos) {
+	void F(grafPont& a, grafPont& b, const bool szomszedos) {
 		const float csillapitas = 0.0001f;
-		if (szomszedos) {	
-			if (normTav(a, b) < (DIST - HIBAHATAR)) { //tul kozel
+		if (szomszedos) {
+			if (normTav(a, b) < (DIST - HIBAHATAR)) {
 				vec3 kul(a.pos - b.pos);
 				kul = kul * csillapitas;
 				a.ero = a.ero + kul;
 			}
-			else if (normTav(a, b) > (DIST + HIBAHATAR)) { //tul messze
+			else if (normTav(a, b) > (DIST + HIBAHATAR)) {
 				vec3 kul(b.pos - a.pos);
 				kul = kul * csillapitas;
 				a.ero = a.ero + kul;
@@ -293,37 +318,58 @@ Graf g;
 
 struct Mozgas {
 	vec2 kezdopont;
-	Mozgas() {}
+	bool kezd;
+	Mozgas():kezd(false) {}
 	void onPress(int px, int py) {
-		kezdopont.x = float(px);
-		kezdopont.y = float(py);
+		kezd = false;
 	}
 	void onMove(int px, int py) {
+		if (!kezd) {
+			kezd = true;
+			kezdopont.x = (float(px) - 300.0f) / 300.0f;
+			kezdopont.y = (float(py) - 300.0f) / 300.0f;
+			return;
+		}
+		kezdopont.x = 0.0f;
+		kezdopont.y = 0.0f;
+		vec3 p = hip(kezdopont);
 		vec2 vegpont = vec2(float(px), float(py));
-		vec3 p = trf(kezdopont);
-		vec3 q = trf(vegpont);
-		float hipTav = d(p,q);
-		if (hipTav > 0.05f) {
-			vec3 v = (q - p * coshf(hipTav)) / sinh(hipTav);
-			vec3 m1 = p * coshf(hipTav / 10) + v * sinh(hipTav / 10);
-			vec3 m2 = p * coshf((hipTav * 6) / 10) + v * sinh((hipTav * 6) / 10);
+		vegpont.x = 0.004f;// (vegpont.x - 300.0f) / 300.0f;
+		vegpont.y = 0.000f;// (vegpont.y - 300.0f) / 300.0f;
+		vec3 q = hip(vegpont);
+
+		const float hipTav = dd(p, q);
+		if (hipTav > 0.001f) {
+			vec3 v = (q - (p * coshf(hipTav))) / (sinhf(hipTav));
+			float tized = hipTav / 10.0f;
+			vec3 m1 = (p * coshf(tized)) + (v * sinhf(tized));
+			float hatod = (hipTav*6.0f) / 10.0f;
+			vec3 m2 = (p * coshf(hatod)) + (v * sinhf(hatod));
+
+			printf("PQ: %.5f \t\t  M1M2: %.5f\n", dd(p, q), dd(m1, m2));
+
 			for (size_t i = 0; i < NODES; ++i) {
 				tukrozes(g[i], m1);
 				tukrozes(g[i], m2);
 				g[i].pos = Eukl(g[i].hip);
-				printf("%.3f ; %.3f ; %.3f \n", g[i].pos.x, g[i].pos.y, g[i].pos.z);
 			}
 			kezdopont = vegpont;
 			g.prepareCircle();
 			glutPostRedisplay();
 		}
 	}
+	
 	void tukrozes(grafPont& gp, const vec3& m1) {
-		vec3& p = gp.hip;
-		float pm = d(p, m1);
-		vec3 v = (m1 - p * coshf(pm)) / (sinh(pm));
-		vec3 pvesszo = p * coshf(pm) + v * sinhf( pm);
-		p = pvesszo;
+		vec3 p = hip(vec2(gp.pos.x, gp.pos.y));
+		float pm = dd(p, m1);
+		vec3 v = (m1 - p * coshf(pm)) / sinh(pm);
+		vec3 pvesszo = p * coshf(2.0f*pm) + v * sinhf(2.0f * pm);
+		sikra(pvesszo);
+		gp.hip = pvesszo;
+	}
+		
+	void printVec3(const vec3& out, const char* name ="")const {
+		printf(">%s\t(%.4f ; %.4f ; %.4f)\n",name, out.x, out.y, out.z);
 	}
 
 };
@@ -336,22 +382,20 @@ void onInitialization() {
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
 void onDisplay() {
-	glClearColor(0, 0, 0, 0);     // background color
-	glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
+	glClearColor(0, 0, 0, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
 	g.drawEdges();
 	g.drawCircle();
-	glutSwapBuffers(); // exchange buffers for double buffering
+	glutSwapBuffers();
 }
 void onKeyboard(unsigned char key, int pX, int pY) {
 	if (key == ' ' && dinSim == false) {
-		g.magic(); //heurisztika
+		g.magic();
 		g.prepareCircle();
 		g.prepareEdges();
 		glutPostRedisplay();
 		dinSim =!dinSim;
 	}
-	else if(key == ' ' && dinSim)
-		dinSim =false;
 }
 void onKeyboardUp(unsigned char key, int pX, int pY) {
 }
@@ -359,22 +403,11 @@ void onMouseMotion(int pX, int pY) {
 	mo.onMove(pX, pY);
 }
 void onMouse(int button, int state, int pX, int pY) {
-	if (state == 1 && button == 0) {	//A lenyomás pillanata
-		mo.onPress(pX,pY);
+	if (state == 1 && button == 0) {
+		mo.onPress(pX, pY);
 	}
-	else if (state == 0 && button == 0) {	//A felengedés pillanata
+	else if (state == 0 && button == 0) {
 		mo.onMove(pX, pY);
-	}
-	if (state == 1 && button == 1) {
-		vec3 kiinduloEukl = vec3(0.0f,0.72f,0.0f);
-		printf(">eukl( %.4f ; %.4f ; %.4f ) \n", kiinduloEukl.x, kiinduloEukl.y, kiinduloEukl.z);
-
-		vec3 kiHip = trf(vec2(kiinduloEukl.x , kiinduloEukl.y),1.0f);
-		
-		printf(">hip( %.4f ; %.4f ; %.4f ) \n", kiHip.x, kiHip.y, kiHip.z);
-		kiHip = Eukl(kiHip);
-		printf(">eukl( %.4f ; %.4f ; %.4f ) \n", kiHip.x, kiHip.y, kiHip.z);
-
 	}
 }
 void onIdle() {
@@ -384,8 +417,6 @@ void onIdle() {
 			sum+=g.calcNode(i);
 		for (size_t i = 0; i < NODES; ++i)
 			g[i].repos();
-		if (sum < 0.03f) 
-			dinSim = false;
 		g.prepareCircle();
 		g.prepareEdges();
 		glutPostRedisplay();
